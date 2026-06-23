4e1d31868be0f555c6ea088987436d839bbf74c6
yudhakr <yudhadeux@gmail.com>
2026-06-23 11:22:16 +0800

    pembaruan 4

--- latihan.js
+++ latihan.js
@@ -14,6 +14,7 @@ else {    console.log("Tutup");
 }
 
 //Operasi Aritmatika
+console.log("==== Operasi Aritmatika ===");
 var a = 10;
 var b = 5;
 var penjumlahan = a + b;
@@ -28,6 +29,7 @@ console.log(" Hasil Pembagian: " + pembagian);
 console.log(" Hasil Modulus: " + modulus);
 
 //Operasi Perbandingan
+console.log("==== Operasi Perbandingan ===");
 var x = 1;
 var z = 1;
 var y = 2;
@@ -38,7 +40,7 @@ console.log("x === z: " + (x === z));
 console.log("x != z: " + (x != z));
 
 //Operator Kondisional
-console.log("Operator Kondisional");  
+console.log("==== Operator Kondisional ====");
 console.log(true && true);   // true
 console.log(true || false);  // true
 
@@ -62,3 +64,25 @@ console.log( word + " " + word2);
 var word3 = "javascript";
 console.log(word.substring(0, 3));
 console.log(word2.toUpperCase()); // Mengambil substring dari indeks 0 hingga 3
+
+
+//Mengubah menjadi string
+console.log("==== Mengubah menjadi string ===");
+
+var angka = 123;
+var angkaString = "sepuluh";
+var angkaString = angka.toString();
+console.log(angkaString);
+// .toString() digunakan untuk mengubah tipe data menjadi string.
+console.log(typeof angkaString); // Output: string
+// String(namaVariable) digunakan untuk mengubah tipe data menjadi string.
+console.log(String(angka)); // Output: "123"
+// String(namaVariable) digunakan untuk mengubah tipe data menjadi string.
+console.log(typeof String(angka)); // Output: string
+// Strring(namaVariable)
+console.log(String(angka)); // Output: "123"
+// Mengubah menjadi angka/Number
+console.log(Number(angkaString)); // Output: 123
+// parseInt(namaVariable).
+console.log(parseInt(angkaString)); // Output: 123
+
