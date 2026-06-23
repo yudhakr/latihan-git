e1ea7c626bfb5d93e79efa7b47c03903ec87b8c8
yudhakr <yudhadeux@gmail.com>
2026-06-23 09:21:46 +0800

    permeuan 2

--- latihan.js
+++ latihan.js
@@ -1,1 +1,28 @@
+var hari = "Senin";
+var quota = 10;
+
 console.log("Pengembangan Web Lanjutan - Latihan JavaScript");
+console.log("Hari: " + hari);
+console.log("Kuota: " + quota);
+
+//kode Kondisi
+var open = false;
+if (open == true) {
+    console.log("Buka");
+}
+else {    console.log("Tutup");
+}
+
+//Operasi Aritmatika
+var a = 10;
+var b = 5;
+var penjumlahan = a + b;
+var pengurangan = a - b;
+var perkalian = a * b;
+var pembagian = a / b;
+var modulus = a % b;
+console.log(" Hasil Penjumlahan: " + penjumlahan);
+console.log(" Hasil Pengurangan: " + pengurangan);
+console.log(" Hasil Perkalian: " + perkalian);
+console.log(" Hasil Pembagian: " + pembagian);
+console.log(" Hasil Modulus: " + modulus);
