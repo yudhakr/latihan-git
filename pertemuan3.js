3f2e51f76af0a351860887d2b4f134fb7b684ada
yudhakr <yudhadeux@gmail.com>
2026-06-24 09:02:39 +0800

    pertemuan 3

---
+++ pertemuan3.js
@@ -0,0 +1,8 @@
+console.log("---- Fundamental JavaScript ----");
+var negara ="indonesia";
+if (negara == "jepang") {
+    console.log("negara ini adalah indonesia");
+}
+else {
+    console.log("negara ini bukan indonesia");
+}
