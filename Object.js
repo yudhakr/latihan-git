d0d9aeb6b534c200fbcb1d98fdd9053d8f704ddb
yudhakr <yudhadeux@gmail.com>
2026-06-25 10:49:32 +0800

    Array object

---
+++ Object.js
@@ -0,0 +1,47 @@
+//  console.log("==== Deklarasi Object ====");
+//  var car = {
+//      brand: "Toyota",
+//      type: "Avanza",
+//      price: 50000000,
+//      "house power": 986
+//  };
+//  console.log(car);
+
+// var orang = {
+//     nama: "Ayudha Kusuma Rahmadhani",
+//     jenisKelamin: "Laki-laki",
+//     tinggibadan: 170,
+//     berat: 60,
+//     usia: 25,
+// }
+// console.log(orang);
+// console.log(orang.nama);
+// console.log(orang.jenisKelamin);
+ 
+
+// console.log("==== Array Object ====");
+// var orang2 = [ {
+//     nama: "Ayudha Kusuma Rahmadhani",
+//     jenisKelamin: "Laki-laki",
+//     tinggibadan: 170,
+//     berat: 60,
+//     usia: 25
+// },
+// {
+//     nama: "Budi Setiawan",
+//     jenisKelamin: "Laki-laki",
+//     tinggibadan: 175,
+//     berat: 70,
+//     usia: 30
+// },
+// {
+//     nama: "Citra Dewi",
+//     jenisKelamin: "Perempuan",
+//     tinggibadan: 165,
+//     berat: 55,
+//     usia: 28
+// }
+//  ];
+// // console.log(orang2);
+// console.log(orang2[1].nama);
+// console.log(orang2[2].nama);
