20584d19b8655ff766749548d02f0b525f1af46d
yudhakr <yudhadeux@gmail.com>
2026-06-25 08:43:43 +0800

    pertemuan hari 4 merapihkan #11

---
+++ Pertemuan-H4.js
@@ -0,0 +1,13 @@
+//Looping Array
+console.log("==== Looping Array  Contoh 1====");
+var arr = ['apel', 'jeruk', 'mangga', 'pisang'];
+for (var i = 0; i < 8; i++) {
+    console.log(arr[i]);
+}
+
+// // contoh 2
+console.log("==== Looping Array  Contoh 2====");
+var names = ["john", "doe", "jack"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
