47494df5768cef1b0fcb1d5689b2213d8f32b9e2
yudhakr <yudhadeux@gmail.com>
2026-06-25 13:51:00 +0800

    pembaruab Currying Function #15

--- Function-Programming.js
+++ Function-Programming.js
@@ -11,31 +11,34 @@
 // }
 
 // countDown(4);
-console.log( "=== first-class function ===" );
 
-function HitungLingkaran(radius) {
-  var pi = 3.14;
 
-  // Fungsi untuk lingkaran
-  function luas(r) { return pi * r * r; }
-  function keliling(r) { return 2 * pi * r; }
+
+// console.log( "=== first-class function ===" );
+
+// function HitungLingkaran(radius) {
+//   var pi = 3.14;
+
+//   // Fungsi untuk lingkaran
+//   function luas(r) { return pi * r * r; }
+//   function keliling(r) { return 2 * pi * r; }
   
-  console.log("Luas Lingkaran:", luas(radius));
-  console.log("Keliling Lingkaran:", keliling(radius));
-}
+//   console.log("Luas Lingkaran:", luas(radius));
+//   console.log("Keliling Lingkaran:", keliling(radius));
+// }
 
-// Nama fungsi diubah dari Persegi menjadi HitungPersegi
-function HitungPersegi(sisi) {
-  // Fungsi untuk persegi
-  function luaspersegi(s) { return s * s; } 
-  function kelilingpersegi(s) { return 4 * s; }
+// // Nama fungsi diubah dari Persegi menjadi HitungPersegi
+// function HitungPersegi(sisi) {
+//   // Fungsi untuk persegi
+//   function luaspersegi(s) { return s * s; } 
+//   function kelilingpersegi(s) { return 4 * s; }
 
-  console.log("Luas Persegi :", luaspersegi(sisi));
-  console.log("Keliling Persegi :", kelilingpersegi(sisi));
-}
+//   console.log("Luas Persegi :", luaspersegi(sisi));
+//   console.log("Keliling Persegi :", kelilingpersegi(sisi));
+// }
 
-HitungLingkaran(7);
-HitungPersegi(5); // Sekarang pemanggilan ini akan berhasil
+// HitungLingkaran(7);
+// HitungPersegi(5); // Sekarang pemanggilan ini akan berhasil
 
 
 // console.log ("=== Currying Function ===")
@@ -51,4 +54,21 @@ HitungPersegi(5); // Sekarang pemanggilan ini akan berhasil
 
 // HitungLingkaran(8)
 
+
+console.log( "=== Currying Function ===" );
+
+function tambah (a, b){
+  return a + b
+}
+
+console.log(tambah(2, 5)) // 7
+
+// currying function
+function tambah (a){
+  return function(b){
+    return a + b
+  }
+}
+
+console.log(tambah(2)(5)) // 7
 
