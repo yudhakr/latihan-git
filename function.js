98dda0beedf8d96892f7dc1b9e50b9704be32213
yudhakr <yudhadeux@gmail.com>
2026-06-25 09:39:26 +0800

    menjalankan function

---
+++ function.js
@@ -0,0 +1,10 @@
+console.log("==== Implementasi Function ====");
+function tampilkan(nama = "Asep", nama2 = "Saputra") {
+    console.log("Hallo, " + nama + " " + nama2);
+}
+tampilkan();
+
+//  console.log("==== Contoh 2: Function dengan Parameter ====");
+// function angka() {
+//     return 2
+// }
