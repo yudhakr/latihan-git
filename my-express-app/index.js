9d955a98af1254c1f01b9be55bc05087bfc8541c
yudhakr <yudhadeux@gmail.com>
2026-07-03 11:03:54 +0800

    midware #23

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -14,17 +14,17 @@ let movies = [
 {id: 5, title: "Para Pencari Tuhan", year: 2026}
 ]
 
-const getMovies = (req, res) => {
-    let { title } = req.query
+// const getMovies = (req, res) => {
+//     let { title } = req.query
 
-    console.log(title)
+//     console.log(title)
 
-    let result = "";
-    movies.forEach((item, index) => {
-        result += `<h3>${index + 1}. ${item.title} (${item.year})</h3>`;
-    });
-    res.send(result);
-}
+//     let result = "";
+//     movies.forEach((item, index) => {
+//         result += `<h3>${index + 1}. ${item.title} (${item.year})</h3>`;
+//     });
+//     res.send(result);
+// }
 
 // Method untuk mendapatkan file berdasarkan id
 const getMovieById = (req, res) => {
@@ -45,7 +45,7 @@ const getMoviesApi = (req, res) => {
 
     console.log(title)
 
-    if (title == undefined) {
+    if (title === undefined){ 
         title = ""
     }
     let result = movies.filter((item) =>{
@@ -67,12 +67,46 @@ const getMovieByIdApi = (req, res) => {
  res.send(`<h3>${result.title} (${result.year})</h3>`);
 }
 
-app.get('/movies', getMovies);
+
+const loggerMiddleware = (req, res, next) => {
+    console.log(`Method: ${req.method}`)
+    console.log(`URL: ${req.url}`)
+    next()
+
+}
+const tokenMiddleware = (req, res, next) => {
+  let { token } = req.query
+
+  if (token === "1200") {
+    next()
+  } else {
+    res.status(401).json({
+      message: "Token tidak valid"
+    })
+  }
+}
+const checkMovieIdMiddleware = (req, res, next) => {
+  let { id } = req.params
+  let result = movies.find(item => item.id === Number(id))
+
+  if (result) {
+    next()
+  } else {
+    res.status(404).json({ message: "Movie tidak ditemukan" })
+  }
+}
+
+
+
+
+app.get('/api/movies', loggerMiddleware, tokenMiddleware, getMoviesApi)
+
+// app.get('/movies', getMovies);
 app.get('/movies/:id', getMovieById);
 
 
-app.get('/api/movies', getMoviesApi);
-app.get('/api/movies/:id', getMovieByIdApi);
+// app.get('/api/movies', getMoviesApi);
+app.get('/api/movies/:id',loggerMiddleware, checkMovieIdMiddleware, getMovieByIdApi);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
