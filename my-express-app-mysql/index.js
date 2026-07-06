a100abd5d991031782e3c183be9d6140dcacc9c4
yudhakr <yudhadeux@gmail.com>
2026-07-06 16:32:04 +0800

    pembelajaran JWT #29

--- my-express-app-mysql/index.js
+++ my-express-app-mysql/index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const movieRouter  = require('./src/routes/movieRouter');
+const cors = require('cors');
 
 
+var corsOptions = {
+  origin: 'http://localhost:3000', // Ganti dengan domain yang diizinkan
+  optionsSuccessStatus: 200 // Untuk beberapa browser lama
+};
+
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api', movieRouter);
 
---
+++ my-express-app-mysql/src/routes/userRouter.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const movieRouter = express.Router()
+
+userRouter.get('/login',login)
+userRouter.get('/register',register)
+
+
+module.exports = userRouter
